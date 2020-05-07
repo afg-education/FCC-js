583cee98e724b29c335cede3ac194f862c9d70f9
[afgonullu] <afgonullu@gmail.com>
2020-05-08 00:50:59 +0300

    intermediate 13

---
+++ Intermediate/13-sum-all-primes.js
@@ -0,0 +1,26 @@
+function sumPrimes(num) {
+  let result = 0;
+  let s = 2;
+
+  while (s <= num) {
+    if (isPrime(s)) {
+      console.log(s);
+      result += s;
+    }
+    s++;
+  }
+
+  console.log(result);
+  return result;
+
+
+  function isPrime(number) {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) return false;
+    }
+    return true;
+  }
+
+}
+
+sumPrimes(977);
